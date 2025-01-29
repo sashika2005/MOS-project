d8642ffe7743313585beffaa8ae96d52ba6039a7
Tharu003 <ireshasandya74616@gmail.com>
2025-01-29 11:14:59 +0530

    aaaaaa

--- submit.php
+++ submit.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Execute and check if inserted
     if ($stmt->execute()) {
-        echo "<script>alert('Registration successful!'); window.location.href='form.php';</script>";
+        echo "<script>alert('Registration successful!'); window.location.href='displaytable.php';</script>";
     } else {
         echo "<script>alert('Error: Could not register.'); window.history.back();</script>";
     }
